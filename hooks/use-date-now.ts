fcb96c98fc9af4cf8d5c871dc2582d88d96cc9b8
asyomei <vabgalimov@vivaldi.net>
2024-01-21 23:14:25 +0500

    Fix setInterval lifecycle

--- hooks/use-date-now.ts
+++ hooks/use-date-now.ts
@@ -3,9 +3,13 @@ import { type Ref, onUnmounted, ref } from "vue"
 export function useDateNow(): Ref<string> {
   const now = ref(new Date())
 
-  const interval = setInterval(() => {
-    now.value = new Date()
-  }, 1000)
+  let interval: NodeJS.Timeout
+
+  onMounted(() => {
+    interval = setInterval(() => {
+      now.value = new Date()
+    }, 1000)
+  })
 
   onUnmounted(() => clearInterval(interval))
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,7 @@ export default defineNuxtConfig({
     strict: true,
     shim: false,
   },
+  nitro: {
+    preset: "github-pages",
+  },
 })
