8872f8baa38ddd0574dd460a15ba4232c2af6f99
vadim abgalimov <vadim@asyomei.org>
2024-11-02 11:00:22 +0500

    chore: change site url

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: "https://asyomei.github.io",
-	devToolbar: { enabled: false },
-	vite: {
-		define: {
-			"import.meta.env.VITE_BUILD_DATE": JSON.stringify(
-				new Date().toISOString().split("T")[0],
-			),
-		},
-	},
+  site: "https://asyomei.org",
+  devToolbar: { enabled: false },
+  vite: {
+    define: {
+      "import.meta.env.VITE_BUILD_DATE": JSON.stringify(getBuildDate()),
+    },
+  },
 });
+
+function getBuildDate(): string {
+  const date = new Date().toISOString();
+  return date.slice(0, date.indexOf("T"));
+}
